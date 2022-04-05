1d451ac132d7cd51553ee143ca274be41a398493
JedwardMook <jedwardmook@yahoo.com>
2022-04-05 11:42:28 -0500

    commit tree 660dae7650e86c35248be135dda900c07cfeb9f0

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,25 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+class ShoppingList extends React.Component{
+    
+  state = {
+      selectedCategory: "All"
+    }
+  
 
-  function handleCategoryChange(event) {
-    // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
+  handleCategoryChange = (event) => {
+    this.setState({selectedCategory: event.target.value});
   }
 
+  render(){
   // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    const itemsToDisplay = this.props.items.filter((item) => {
+    if (this.state.selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
+    return item.category === this.state.selectedCategory;
   });
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
+        <select name="filter" onChange={this.handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -34,5 +38,5 @@ function ShoppingList({ items }) {
     </div>
   );
 }
-
+}
 export default ShoppingList;
